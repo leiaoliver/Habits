a23130658b31d89b7e528646e7c93ba67c903353
Leia Oliveira <leiaoliver388@gmail.com>
2023-01-20 22:51:09 -0300

    habits

---
+++ server/src/server.ts
@@ -0,0 +1,30 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+/**
+ * Metodo HTTP: Get(buscar informação), Post(criar algo), Put(atualizar recurso), Patch(atualizar recurso especifico),Delete(deletar)
+ */
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "Beber",
+      },
+    },
+  });
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running");
+  });
