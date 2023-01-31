a440a66b8e83084d85d7c464c9a8db12063b98d3
Léia <leiaoliver388@gmail.com>
2023-01-31 15:10:05 -0300

    update bd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import {prisma} from './lib/prisma'
 
 const app = Fastify();
-const prisma = new PrismaClient();
+
 
 app.register(cors);
 /**
