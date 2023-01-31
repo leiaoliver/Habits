a2c980d61f21a616d85d8bcf0c390068f4b2efa9
Léia <leiaoliver388@gmail.com>
2023-01-31 16:23:32 -0300

    create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,19 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import {prisma} from './lib/prisma'
+import { appRoutes } from "./routes";
 
 const app = Fastify();
 
-
 app.register(cors);
+app.register(appRoutes);
 /**
  * Metodo HTTP: Get(buscar informação), Post(criar algo), Put(atualizar recurso), Patch(atualizar recurso especifico),Delete(deletar)
  */
 
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "Beber",
-      },
-    },
-  });
-  return habits;
-});
-
 app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log("HTTP Server running");
-  });
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP Server running");
+	});
