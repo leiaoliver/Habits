fb23f20b0f6a44ceb82f9f6e253b26e3da0e3004
Léia <leiaoliver388@gmail.com>
2023-02-02 16:43:27 -0300

    created utils

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,12 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDateSize = 18 * 7;
+const amountOfToFill = minimumSummaryDateSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -19,14 +25,19 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+
+        {amountOfToFill > 0 &&
+          Array.from({ length: amountOfToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-800 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
